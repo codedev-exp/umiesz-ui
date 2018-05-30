1be84a1c7730c29cb08ca5a0035e576b85a9edc2
codedev-exp <p.truchann@gmail.com>
2018-05-30 15:17:43 +0200

    Save before ejecting

--- umiesz-app/src/index.js
+++ umiesz-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { promiseMiddleware } from './middleware';
-import './index.css';
+import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
